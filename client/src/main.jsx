e27336b3ed95da89c847c299c8274193084f17b7
abinandd <112941816+abinandd@users.noreply.github.com>
2025-07-28 22:13:50 +0530

    Initial commit

---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import React ,{ StrictMode }  from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter ,Routes,Route} from "react-router";
+import ChatBox from "./components/ChatBox.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/chat" element={<ChatBox/>}/>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
