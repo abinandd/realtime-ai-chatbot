cad753f753dd768745a602cc394ce1076ef2d76e
abinandd <112941816+abinandd@users.noreply.github.com>
2025-07-30 15:22:08 +0530

    link update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 const server = createServer(app);
 const io = socketIO(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://realtime-ai-chatbot.vercel.app/",
   },
 });
 
